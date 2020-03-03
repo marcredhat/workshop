d7729733b77d5bc68527be156fb0e9b3231b0940
Marc Chisinevski <mchisine@redhat.com>
2020-03-03 13:33:17 -0800

    Update ocpnetworkdiagram.py

--- diagramsascode/ocpnetworkdiagram.py
+++ diagramsascode/ocpnetworkdiagram.py
@@ -4,10 +4,13 @@ from diagrams.aws.database import ElastiCache, RDS
 from diagrams.aws.network import ELB
 from diagrams.aws.network import Route53
 from diagrams.onprem.network import Haproxy
+from diagrams.k8s.rbac import User
+from diagrams.k8s.network import Ingress
+from diagrams.onprem.compute import Server
 
 with Diagram("OpenShift 4.3 with external load balancer", show=False):
-    dns = Route53("dns")
-    lb = ELB("F5")
+    dns = Route53("DNS")
+    lb = ELB("External load balancer (F5 BIG-IP etc")
     internallb = Haproxy("Internal load balancer")
 
     with Cluster("Masters"):
@@ -31,8 +34,24 @@ with Diagram("OpenShift 4.3 with external load balancer", show=False):
         storage = RDS("Container registry")
         storage - [RDS("Apps")]
 
+        
+    with Cluster("Developers and Admins"):
+        devs_and_admins = [User("Developers"),
+                     User("Admins"))]
+        
+    with Cluster("Users"):
+        users= [User("Users")]
+        
+    with Cluster("Operations VLAN"):
+        operations_VLAN = [Server("GitlabCI"),
+                           Server("Jenkins"),
+                           Server("Bamboo"),
+                           Server("LDAP"),
+                           Server("NTP"),
+                           Server("SMTP")]
+        
 
-    internallb >> master_group
+    devs_and_admins >> operations_VLAN >> internallb >> master_group
     internallb >> infra_group >> storage
-    dns >> lb >> worker_group >> storage
+    users >> dns >> lb >> worker_group >> storage
     worker_group >> ca_bundle
